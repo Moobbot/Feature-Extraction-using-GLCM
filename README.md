b8269c417e3c4123c7b60a7ac37018c79121a5c9
Ngô Tâm <155130804+TamAIRC@users.noreply.github.com>
2025-02-17 22:25:13 +0700

    Add images and requirements for feature extraction project

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Feature-Extraction-using-GLCM
-Extracting Features using GLCM for Single Image and Multiple Image from a folder
 
+Extracting Features using GLCM for Single Image and Multiple Image from a folder
 
 Gray Level Co-occurrence Matrix Method (GLCM)
 
@@ -14,106 +14,102 @@ Describe the joint probability of two pixel values ​​separated by d appearin
 
 Several concepts:
 
-* Step size n
+- Step size n
 
-    * The distance from the center pixel (x, y) (number of pixels)
+  - The distance from the center pixel (x, y) (number of pixels)
 
-* Direction α
+- Direction α
 
-    * Four directions
+  - Four directions
 
-        0°(0,d) （x,y+d）
-        
-        45°（d,-d） (x+d,y-d)
-        
-        90°（d,0） (x+d,y)
-        
-        135°（-d,-d） (x-d,y-d)
-        
-        
+    0°(0,d) （x,y+d）
+
+    45°（d,-d） (x+d,y-d)
+
+    90°（d,0） (x+d,y)
+
+    135°（-d,-d） (x-d,y-d)
+
 Matrix order:
 
     * The maximum gray level of the image, such as an 8-bit grayscale image, the maximum gray level is 256, then the shape of the co-occurrence matrix is ​​256*256
 
+Solution steps: 1. Determine the number of gray levels of the image n\*n
 
-Solution steps:
-    1. Determine the number of gray levels of the image n*n
-    
     2. Determine the matrix order n
-    
+
     3. Determine the direction and step length
-    
+
     4. Count the frequency of pixel pairs of adjacent pixel pairs according to the given method
-    
+
     5. Convert frequency to frequency or probability
-    
-    
+
 Texture feature extraction:
 
-* Angle second moment (energy) ASM
+- Angle second moment (energy) ASM
 
-    * Measure whether the grayscale distribution is uniform or not, whether the texture is thick or not, even & fine, with large
-    
-* Correlation COR
+  - Measure whether the grayscale distribution is uniform or not, whether the texture is thick or not, even & fine, with large
 
-    * The gray scale of rows and columns has a large linear relationship, and the value is large
+- Correlation COR
 
-* Contrast CON
+  - The gray scale of rows and columns has a large linear relationship, and the value is large
 
-    * Sharp edges, large color difference, large value
+- Contrast CON
 
-* Differential DISL
+  - Sharp edges, large color difference, large value
 
-    * Large local contrast, large value
+- Differential DISL
 
-* Entropy
+  - Large local contrast, large value
 
-    * Image complexity, large complexity value
+- Entropy
 
-* Inverse difference matrix HOMO
+  - Image complexity, large complexity value
 
-    * The image texture is clear and regular, the clearer the more regular the value is
+- Inverse difference matrix HOMO
 
-*Significance*
+  - The image texture is clear and regular, the clearer the more regular the value is
 
-* The performance of adjacent pixels correlation
+_Significance_
 
-* The diagonal elements of the co-occurrence matrix reflect the degree of gray change
+- The performance of adjacent pixels correlation
 
-    * The diagonal is deviated to 0, the gray level in the specified direction is less repetitive and the change is large
-    
-    * The diagonal element value deviates from 0, the change is small
+- The diagonal elements of the co-occurrence matrix reflect the degree of gray change
+
+  - The diagonal is deviated to 0, the gray level in the specified direction is less repetitive and the change is large
+
+  - The diagonal element value deviates from 0, the change is small
 
 Pros and cons
 
-* Advantage:
+- Advantage:
 
-    -Simple method
-    
-     Easy to implement
-    
-     GLCM method is recognized as an effective method
-     
-     It has strong adaptability and robustness.
-     
-* Disadvantages:
+  -Simple method
 
-    It is out of touch with the human visual model, lacks the use of global information, and it is difficult to study the inheritance or dependence of pixels between texture scales
-    
-    Lack of theoretical support
-    
-    The computational complexity is very high, which restricts its practical application
+  Easy to implement
+
+  GLCM method is recognized as an effective method
+
+  It has strong adaptability and robustness.
+
+- Disadvantages:
+
+  It is out of touch with the human visual model, lacks the use of global information, and it is difficult to study the inheritance or dependence of pixels between texture scales
+
+  Lack of theoretical support
+
+  The computational complexity is very high, which restricts its practical application
 
 Python implementation
 
 Step:
 
     1. Grayscale the image
-    
+
     2. The image is converted to 8-bit unsigned integer
-    
+
     3. Perform feature compression on the image, compress 256 levels of gray to 16 levels
-    
+
     4. Solve the co-occurrence matrix
-    
+
     5. Get each texture feature
---
+++ setup.py
@@ -0,0 +1,169 @@
+import io
+import logging
+import platform
+import zipfile
+import os
+import subprocess
+
+try:
+    import requests
+except ModuleNotFoundError as e:
+    # print("Requests module not found:", e)
+    subprocess.run(["pip", "install", "requests"])
+    import requests
+
+# Configure logging
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+
+def download_file(url, folder, filename):
+    """
+    Download a file from a URL and save it to a specified folder.
+
+    Args:
+    - url (str): URL of the file to download.
+    - folder (str): Path to the folder where the file will be saved.
+    - filename (str): Name of the file to be saved.
+
+    Returns:
+    - str: Full path of the downloaded file, or None if the file already exists.
+    """
+
+    # Tạo đường dẫn đầy đủ của tệp
+    filepath = os.path.join(folder, filename)
+
+    # Kiểm tra xem tệp đã tồn tại không
+    if os.path.exists(filepath):
+        print("Tệp đã tồn tại:", filepath)
+        return filepath
+
+    # Tạo thư mục nếu nó chưa tồn tại
+    if not os.path.exists(folder):
+        os.makedirs(folder)
+
+    try:
+        response = requests.get(url)
+        response.raise_for_status()
+        with open(filepath, "wb") as f:
+            f.write(response.content)
+        logger.info(f"File downloaded and saved to: {filepath}")
+        return filepath
+    except requests.RequestException as e:
+        logger.error(f"Error downloading file: {e}")
+        return None
+
+
+def download_and_extract_zip(url, extract_path="."):
+    """
+    Download a ZIP file from the given URL and extract its contents.
+
+    Args:
+    - url (str): The URL of the ZIP file to download.
+    - extract_path (str): The path where the contents of the ZIP file will be extracted. Default is the current directory.
+
+    Returns:
+    - bool: True if the download and extraction were successful, False otherwise.
+    """
+    try:
+        # Check if the destination folder exists, if not, create it
+        if not os.path.exists(extract_path):
+            os.makedirs(extract_path)
+
+        # Get the filename from the URL
+        filename = url.split("/")[-1]
+
+        # Check if the file already exists in the destination folder
+        if os.path.exists(os.path.join(extract_path, filename)):
+            print(f"{filename} already exists. Skipping download.")
+            return True
+
+        # Download the ZIP file
+        logger.info(f"Downloading {filename}...")
+        response = requests.get(url)
+        response.raise_for_status()
+
+        with zipfile.ZipFile(io.BytesIO(response.content), "r") as zip_ref:
+            zip_ref.extractall(extract_path)
+        logger.info(f"{filename} downloaded and extracted successfully.")
+        return True
+    except (requests.RequestException, zipfile.BadZipFile, Exception) as e:
+        logger.error(f"An error occurred: {e}")
+        return False
+
+
+def check_gpu():
+    """
+    Kiểm tra xem máy có GPU hay không mà không cần PyTorch.
+    Hỗ trợ Windows, Linux và macOS.
+    """
+    try:
+        if platform.system() == "Windows":
+            output = subprocess.check_output(
+                ["wmic", "path", "win32_VideoController", "get", "name"],
+                universal_newlines=True,
+            )
+            gpus = [
+                line.strip()
+                for line in output.split("\n")
+                if line.strip() and "Name" not in line
+            ]
+        elif platform.system() == "Linux":
+            output = subprocess.check_output(["lspci"], universal_newlines=True)
+            gpus = [
+                line
+                for line in output.split("\n")
+                if "VGA" in line or "3D controller" in line
+            ]
+        elif platform.system() == "Darwin":  # macOS
+            output = subprocess.check_output(
+                ["system_profiler", "SPDisplaysDataType"], universal_newlines=True
+            )
+            gpus = [
+                line.strip() for line in output.split("\n") if "Chipset Model" in line
+            ]
+        else:
+            print("Không xác định được hệ điều hành.")
+            return None
+        return gpus if gpus else None
+    except Exception as e:
+        print(f"Lỗi khi kiểm tra GPU: {e}")
+        return None
+
+
+# torch==1.13.1
+# torchio==0.18.74
+# torchvision==0.14.1
+def install_packages():
+    # print("Install Torch")
+    # gpus = check_gpu()
+    # packages = ["torch", "torchvision", "torchaudio"]
+    # if gpus:
+    #     print("GPU detected:")
+    #     for gpu in gpus:
+    #         print(f" - {gpu}")
+    #     print("Installing PyTorch with CUDA support...")
+    #     subprocess.run(
+    #         [
+    #             "pip",
+    #             "install",
+    #             *packages,
+    #             "--index-url",
+    #             "https://download.pytorch.org/whl/cu121",
+    #         ]
+    #     )
+    # else:
+    #     print("No GPU detected, installing CPU version of PyTorch...")
+    #     subprocess.run(["pip", "install", "torch==1.13.1", "torchvision==0.18.74", "torchaudio==0.14.1"])
+
+    print("Install requirements")
+    subprocess.run(["pip", "install", "-r", "requirements.txt"])
+
+
+def main():
+    print("install_packages")
+    install_packages()
+
+
+if __name__ == "__main__":
+    main()
